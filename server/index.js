b7addcf9638cda2d7a1a42972f05071897059168
KJAVA19811815 <karunsingh1993@gmail.com>
2017-08-07 21:23:24 +0000

    oauth refactor

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("auth/google/callback", passport.authenticate("google"));
 
 app.listen(8080, () => {
   console.log("server at 8080");
