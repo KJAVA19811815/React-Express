622aad7d08c3994aaf16fc461000363bd7e80d0d
KJAVA19811815 <karunsingh1993@gmail.com>
2017-08-10 22:55:14 +0000

    working on auth with passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get(
   "/auth/google",
@@ -13,7 +29,7 @@ app.get(
   })
 );
 
-app.get("auth/google/callback", passport.authenticate("google"));
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(8080, () => {
   console.log("server at 8080");
