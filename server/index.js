5c6d3c534defdb34f0896a3a0bc58aa64168f69c
KJAVA19811815 <karunsingh1993@gmail.com>
2017-09-02 01:59:55 +0000

    stripe setup, for charging the customer

--- server/index.js
+++ server/index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
+const bodyParser = require('body-parser');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-const keys = require("./config/keys");
+// const stripe = require('stripe');
+const keys = require("./config/keys")
+const stripe = require('stripe')(keys.stripeSecretKey);
 require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -39,13 +44,32 @@ app.get(
 
 app.get("/api/logout", (req, res) => {
   req.logout();
-  // res.send(req.user);
   res.redirect('/');
 });
 
 app.get("/api/current_user", (req, res) => {
   console.log(req.user);
   res.send(req.user);
+});
+
+app.post('/api/stripe', (req,res) => {
+  if(!req.user){
+    return res.status(401);
+  } else {
+    stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '5 f0r 5',
+      source: req.body.id
+    },function(err, charge){
+      console.log(charge);
+    })
+    .then(() => {
+        req.user.credits += 5;
+        return req.user.save()
+      })
+    .then((user) => res.send(user))
+  }
 });
 
 app.listen(8080, () => {
