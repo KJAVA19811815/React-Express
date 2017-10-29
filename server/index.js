5ae826787092d8b7f1be27311cafb654687e6fd6
KJAVA19811815 <karunsingh1993@gmail.com>
2017-10-29 23:16:39 +0000

    redux pratice

--- server/index.js
+++ server/index.js
@@ -78,11 +78,15 @@ app.post("/api/stripe", requireLogin, (req, res) => {
 app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
   const { title, subject, body, recipients } = req.body;
 
-  const survey = new Survye{(
+  const survey = new Survey{(
     title: title,
     subject: subject,
     body: body,
-    
+    recipients: recipients.split(',').map(email => {
+      return {email: email.trim()},
+    }),
+    _user: req.user.id,
+    dateSent: Date.now()
   )}
 });
 
